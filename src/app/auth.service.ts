2366b01e8c7cdbf8b0a05ea80088f130f1ed9cce
Rolkyssoh <juventussoh@gmail.com>
2023-08-22 19:52:56 +0100

    The firt commit

---
+++ src/app/auth.service.ts
@@ -0,0 +1,22 @@
+export class AuthService {
+    loggedIn = false;
+
+    isAuthenticated() {
+        const promise = new Promise(
+            (resole, reject) => {
+                setTimeout(() => {
+                    resole(this.loggedIn)
+                }, 800);
+            }
+        );
+        return promise
+    }
+
+    login() {
+        this.loggedIn = true;
+    }
+
+    logout() {
+        this.loggedIn = false;
+    }
+}
